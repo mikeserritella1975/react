b663764d1d53cd43b12031376b552505eaac1a69
mikeserritella1975 <74471710+mikeserritella1975@users.noreply.github.com>
2021-02-21 22:41:08 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+console.log ("mike is cool");
+ReactDOM.render(<p>mike is cool</p>,document.getElementById("root"));
